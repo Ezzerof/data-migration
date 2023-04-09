94ef98f80f9f9845ce5bc18d1f9aec086aceb79a
Ezzerof <christian.bitca@gmail.com>
2023-04-09 20:05:08 +0100

    added Loger to ConnectionProvider

--- src/main/java/com/dataMigration/database/ConnectionProvider.java
+++ src/main/java/com/dataMigration/database/ConnectionProvider.java
@@ -1,5 +1,9 @@
 package com.dataMigration.database;
 
+import com.dataMigration.App;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -13,6 +17,7 @@ public class ConnectionProvider {
     private static final String URL = "jdbc:mysql://localhost:3306/employeemigration";
     private static Connection connection;
     private static Properties properties = new Properties();
+    private static final Logger LOGGER = LogManager.getLogger(App.class);
 
     private ConnectionProvider() {
     }
@@ -23,9 +28,9 @@ public class ConnectionProvider {
                 properties.load(new BufferedReader(new FileReader("src\\main\\resources\\login.properties")));
                 connection = DriverManager.getConnection(URL, properties.getProperty("username"), properties.getProperty("password"));
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                LOGGER.error(e);
             } catch (SQLException e) {
-                throw new RuntimeException(e);
+                LOGGER.error(e);
             }
         }
         return connection;
@@ -36,7 +41,7 @@ public class ConnectionProvider {
             try {
                 connection.close();
             } catch (SQLException e) {
-                throw new RuntimeException(e);
+                LOGGER.error(e);
             }
         }
     }
